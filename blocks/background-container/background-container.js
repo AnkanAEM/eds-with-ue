09a8615c7fc22ac3ec96c147b3c661a296c9d091
Ankan Ghosh <ankan.ghosh@axeno.co>
2025-05-27 13:07:04 +0530

    fix linting

--- blocks/background-container/background-container.js
+++ blocks/background-container/background-container.js
@@ -1,7 +1,5 @@
 export default async function decorate(block) {
   // Clear block content
   block.classList.add('background-container');
-  
-//   block.append(backgroundDiv, contentDiv);
-
+  // block.append(backgroundDiv, contentDiv);
 }
