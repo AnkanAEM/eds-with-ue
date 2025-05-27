ce2970243441c5281b8e49598e90d7b40ec9df6d
Ankan Ghosh <ankan.ghosh@axeno.co>
2025-05-27 15:06:36 +0530

    Changes

--- sections/background-container/background-container.js
+++ sections/background-container/background-container.js
@@ -1,5 +1,5 @@
 export default async function decorate(block) {
-  console.log('decorate background-container block', block);
+  console.log('decorate background-container block section', block);
   // Clear block content
   block.classList.add('background-container');
   // block.append(backgroundDiv, contentDiv);
