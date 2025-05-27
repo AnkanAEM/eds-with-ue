0a2c8fd494828508baf50df56b94ccb0840d9b4c
Ankan Ghosh <ankan.ghosh@axeno.co>
2025-05-27 15:20:44 +0530

    Remove background container

--- blocks/background-container/background-container.js
+++
@@ -1,6 +0,0 @@
-export default async function decorate(block) {
-  console.log('decorate background-container block', block);
-  // Clear block content
-  block.classList.add('background-container');
-  // block.append(backgroundDiv, contentDiv);
-}
--- sections/background-container/background-container.js
+++
@@ -1,6 +0,0 @@
-export default async function decorate(block) {
-  console.log('decorate background-container block section', block);
-  // Clear block content
-  block.classList.add('background-container');
-  // block.append(backgroundDiv, contentDiv);
-}
