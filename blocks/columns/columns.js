c37d8fdc794295bcc7e4207ebd4a4e8af9898b57
Ankan Ghosh <ankan.ghosh@axeno.co>
2025-05-30 15:35:29 +0530

    Fix linting issues

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,9 +1,9 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
-  debugger
+
   // setup image columns
-  [...block.children].forEach((row, count) => {
+  [...block.children].forEach((row) => {
     row.style.cssText = `grid-template-columns: repeat(${row.childElementCount}, 1fr);`;
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
